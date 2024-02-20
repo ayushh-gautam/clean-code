2b4eb9202fb1a76b8cd5f3b54954b4e2c01b3a8f
ayushh-gautam <ayushgautam896@gmail.com>
2024-02-20 22:21:35 +0545

    minor change

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
       splitScreenMode: true,
       builder: (_, child) {
         return MaterialApp(
-          title: 'Foodmandu Rider',
+          title: 'ayush don',
           onGenerateRoute: generateRoute,
           navigatorKey: gNavigatorKey,
           theme: ThemeHelper().generateApptheme(
